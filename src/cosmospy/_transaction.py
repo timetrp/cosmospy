f0b5474938afedf9c44384e9a47614ecd14f3463
time <terapat.chn@gmail.com>
2022-05-23 15:02:20 +0700

    add osmos transaction

--- src/cosmospy/_transaction.py
+++ src/cosmospy/_transaction.py
@@ -14,7 +14,7 @@ import cosmospy.interfaces.coin_pb2 as coin
 import cosmospy.interfaces.msg_send_pb2 as transfer
 import cosmospy.interfaces.pubkey_pb2 as pubkey
 import cosmospy.interfaces.tx_pb2 as tx
-
+import cosmospy.interfaces.tx_osmo as tx_osmo
 
 class Transaction:
     """A Cosmos transaction.
@@ -51,6 +51,24 @@ class Transaction:
         self._sync_mode = sync_mode
         self._tx_body = tx.TxBody()
         self._tx_raw = tx.TxRaw()
+
+    def add_swap_exact_amount_in(
+        self, amount: int, routes, denom: str = "uosmo", hrp: str = "osmo"
+    ) -> None:
+        msg = tx_osmo.MsgSwapExactAmountIn()
+        msg.sender = privkey_to_address(self._privkey, hrp=hrp)
+        for route in routes:
+            _route = tx_osmo.SwapAmountInRoute()
+            _route.poolId = int(route["pool_id"])
+            _route.tokenOutDenom = route["denom"]
+            msg.routes.append(_route)
+        msg.tokenIn.denom = denom
+        msg.tokenIn.amount = str(amount)
+        msg.tokenOutMinAmount = str(amount)
+        msg_any = Any.Any()
+        msg_any.Pack(msg)
+        msg_any.type_url = "/osmosis.gamm.v1beta1.MsgSwapExactAmountIn"
+        self._tx_body.messages.append(msg_any)
 
     def add_transfer(
         self, recipient: str, amount: int, denom: str = "uosmo", hrp: str = "osmo"
