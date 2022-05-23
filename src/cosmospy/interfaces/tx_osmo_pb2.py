7f6f557bfe1f60f7cfacd7091642dd858b46e2aa
time <terapat.chn@gmail.com>
2022-05-23 15:25:53 +0700

    bubmp

--- src/cosmospy/interfaces/tx_osmo_pb2.py
+++ src/cosmospy/interfaces/tx_osmo_pb2.py
@@ -11,9 +11,8 @@ from google.protobuf import symbol_database as _symbol_database
 
 _sym_db = _symbol_database.Default()
 
-
-import gogo_pb2 as gogo__pb2
-import coin_pb2 as coin__pb2
+import cosmospy.interfaces.coin_pb2 as coin__pb2
+import cosmospy.interfaces.gogo_pb2 as gogo__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
